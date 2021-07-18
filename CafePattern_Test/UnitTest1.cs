0918f3f285d0170a91b65709d19769899960bd90
Andra Winters <andra.cierra@gmail.com>
2021-07-18 15:14:41 -0400

    initial

---
+++ CafePattern_Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CafePattern_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
